1e0d558d53d05f05013ba05630541337ef33cfd9
AverySims <avery.j.sims@gmail.com>
2024-01-18 09:47:41 -0600

    Update DivisionOperation.cs
    divide by 0 check

--- Calculator/Assets/Scripts/Calculator/Operators/DivisionOperation.cs
+++ Calculator/Assets/Scripts/Calculator/Operators/DivisionOperation.cs
@@ -4,6 +4,10 @@ namespace CalculatorApp.Operators
 {
 	public class DivisionOperation : ICalculatorOperation
 	{
-		public double Calculate(double a, double b) => a / b;
+		public double Calculate(double a, double b)
+		{
+			if (a == 0 || b == 0) return double.NaN; // return NaN if either operand is 0 (shouldn't divide by 0)
+			return a / b;
+		}
 	}
 }
