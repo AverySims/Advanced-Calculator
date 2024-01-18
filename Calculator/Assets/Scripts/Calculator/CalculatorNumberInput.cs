797f31bdcab4e1ff7ba9193922ce843e4707273f
AverySims <avery.j.sims@gmail.com>
2024-01-18 11:08:38 -0600

    Update CalculatorNumberInput.cs
    added summary for each function

--- Calculator/Assets/Scripts/Calculator/CalculatorNumberInput.cs
+++ Calculator/Assets/Scripts/Calculator/CalculatorNumberInput.cs
@@ -28,6 +28,8 @@ namespace CalculatorApp
 		[SerializeField]
 		private Button signButton;
 
+		#region Unity Methods
+
 		private void OnEnable()
 		{
 			// subscribe to the button click events
@@ -52,19 +54,37 @@ namespace CalculatorApp
 			decimalButton.onClick.RemoveListener(OnDecimalClick);
 			signButton.onClick.RemoveListener(OnSignClick);
 		}
+
+		#endregion
 		
+		/// <summary>
+		/// This method will be called when a digit button (0-9) is clicked.
+		/// </summary>
+		/// <param name="value">The value of the digit button.</param>
 		private void OnDigitClick(int value)
 		{
+			if (buttonEvents != null) buttonEvents.DigitButtonPress(value);
+			
 			Debug.Log($"Digit {value} was clicked!");
 		}
 		
+		/// <summary>
+		/// This method will be called when the decimal button is clicked.
+		/// </summary>
 		private void OnDecimalClick()
 		{
+			if (buttonEvents != null) buttonEvents.DecimalButtonPress();
+			
 			Debug.Log($"Decimal was clicked!");
 		}
 		
+		/// <summary>
+		/// This method will be called when the sign button is clicked.
+		/// </summary>
 		private void OnSignClick()
 		{
+			if (buttonEvents != null) buttonEvents.SignButtonPress();
+			
 			Debug.Log($"Sign was clicked!");
 		}
 		
